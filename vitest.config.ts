0b250b4de12c080882d2bae5797d0b434a4f6a35
sorakinu <maroyuki728@gmail.com>
2026-01-27 12:45:03 +0900

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,13 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+  
   plugins: [react()],
-  test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: ["./src/test/setup.ts"],
-    include: ["src/**/*.{test,spec}.{ts,tsx}"],
-  },
+  base: '/silent-ripple/',
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
   },
